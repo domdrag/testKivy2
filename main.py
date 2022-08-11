fabf5b7f149e394d4f062f7fb91e1f14520fac5d
domdrag <58782481+domdrag@users.noreply.github.com>
2022-08-11 15:04:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,28 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.uix.label import Label
-import urllib3
+
+import os
+import requests
 import pdfplumber
-import io
+import re
 
-def extract_text_from_pdf_by_url(url):
-    all_text = ''
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    
+    with requests.get(url) as r:
+        assert r.status_code == 200, f'error, status code is {r.status_code}'
+        with open(local_filename, 'wb') as f:
+            f.write(r.content)
+        
+    return local_filename
 
-    http = urllib3.PoolManager()
-    temp = io.BytesIO()
-    temp.write(http.request("GET", url).data)
-    try:    # to verify is the url has valid pdf file!
-        pdf = pdfplumber.open(temp)
-        for pdf_page in pdf.pages:
-            single_page_text = pdf_page.extract_text()
-            # TypeError: can only concatenate str (not "NoneType") to str
-            if single_page_text is not None: 
-                all_text += '\n' + single_page_text
-        pdf.close()
-    except:
-        pass
-    return all_text
+invoice = 'https://github.com/domdrag/Python/raw/main/test.pdf'
+invoice_pdf = download_file(invoice)
 
-
-url  = 'https://github.com/domdrag/Python/raw/main/test.pdf'
-text = extract_text_from_pdf_by_url(url)
+pdf = pdfplumber.open(invoice_pdf)
+page = pdf.pages[0]
+text = page.extract_text()
 print(text)
 
 class DataTableApp(App):
