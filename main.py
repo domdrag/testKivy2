dbf317434b3b65f6527ae9b6e7680165debb6d95
domdrag <pasjekonj@gmail.com>
2022-12-31 09:59:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 import kivy
+
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.lang import Builder
 from kivy.uix.label import Label
 
-import os
-import requests
-import pdfplumber
-import re
 
-from jnius import autoclass
-from jnius import cast                      
-                                                       
-Intent = autoclass('android.content.Intent')
-Uri = autoclass('android.net.Uri')          
-PythonActivity = autoclass('org.renpy.android.PythonActivity')                                        
-intent = Intent(Intent.ACTION_CALL)         
-intent.setData(Uri.parse("tel:" + 4565))     
-currentActivity = cast('android.app.Activity', PythonActivity.mActivity)                                                   
-currentActivity.startActivity(intent)
+class MyApp(App):
 
-
-
-text = 'HHHOOOO'
-
-class DataTableApp(App):
     def build(self):
-        return Label(text = text)
+        return Label(text='Hello world')
 
-if __name__=='__main__':
-    DataTableApp().run()
+
+if __name__ == '__main__':
+    MyApp().run()
+
